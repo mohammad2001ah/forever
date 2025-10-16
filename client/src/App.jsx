090f300defa20b3409b9f9080707042d1a3e5133
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-16 17:32:36 +0300

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import{Route, Router,Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import Cart from './pages/Cart';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Order from './pages/Order';
+import Product from './pages/Product';
+import PlaceOrder from './pages/PlaceOrder';
+
+
+
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:-[7vw] lg:-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/product/:id' element={<Product/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   )
-}
+};
 
-export default App
+export default App;
