26d6d1b78e579895461dd5a0dd21800e26905dd9
mohammad2001ah <mah.2001.ah@gmail.com>
2025-10-16 22:33:40 +0300

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,15 +8,16 @@ import Login from './pages/Login';
 import Order from './pages/Order';
 import Product from './pages/Product';
 import PlaceOrder from './pages/PlaceOrder';
-
-
-
+import About from './pages/About';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:-[7vw] lg:-[9vw]'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
         <Route path='/collection' element={<Collection/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/contact' element={<Contact/>}/>
