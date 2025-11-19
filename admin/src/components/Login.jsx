0901952d5f37f9b9495c7567c3657be55b297b1c
mohammad2001ah <mah.2001.ah@gmail.com>
2025-11-19 18:26:03 +0300

    Update Login.jsx

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmitHandler = async(e) => {
+    try {
+      e.preventDefault();
+      
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Admin Panel</h1>
-        <form>
+        <form onSubmit={onSubmitHandler}>
           <div className="mb-6">
             <p className="text-sm font-medium text-gray-700 mb-2">Email Address</p>
-            <input
+            <input  
+            onChange={(e)=>setEmail(e.target.value)}
+            value={email}
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none focus:ring-2 focus:ring-blue-500 transition"
               type="email"
               placeholder="your@email.com"
@@ -17,7 +31,9 @@ const Login = () => {
           </div>
           <div className="mb-6">
             <p className="text-sm font-medium text-gray-700 mb-2">Password</p>
-            <input
+            <input 
+            onChange={(e)=>setPassword(e.target.value)}
+            value={password}
               className="rounded-md w-full px-3 py-2 border border-gray-300 outline-none focus:ring-2 focus:ring-blue-500 transition"
               type="password"
               placeholder="Enter your password"
